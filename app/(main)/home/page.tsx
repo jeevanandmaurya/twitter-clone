f2e5e07cd1940f78efb0addae9812c84292a428c
Jeevanand Maurya <jeevanandmaurya@gmail.com>
2025-12-14 15:54:46 +0530

    linked supabase

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,10 +1,27 @@
 import Link from "next/link";
+import { cookies } from "next/headers";
+import { createClient } from "@/lib/supabase/server";
+
+async function Todos() {
+  const supabase = await createClient();
+
+  const { data: todos } = await supabase.from("todos").select("name");
+  return todos ? (
+    <ol>
+      {todos.map((todo, index) => (
+        <li key={index}>{todo.name}</li>
+      ))}
+    </ol>
+  ) : (
+    <p>No todos found.</p>
+  );
+}
 
 export default function HomePage() {
   return (
     <div className="tabs">
       <div className="following">
-        <h2 >Following</h2>
+        <h2>Following</h2>
         <hr />
         <div id="tweet0" className="tweet">
           <Link href="/profile/user2">User2</Link>
@@ -18,9 +35,11 @@ export default function HomePage() {
         <div id="tweet1" className="tweet">
           <Link href="/profile/user3">User3</Link>
           <p>This is the Third tweet.</p>
+          <br />
+           <p>Here is a list of todos:</p>
+           <Todos />
+            
         </div>
-
-
       </div>
     </div>
   );
